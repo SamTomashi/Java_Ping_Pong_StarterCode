5ca2ad8b9d04ab90e1320f5e0eff4b69f7309f1e
Sam Tomashi <samtomashikayembi@yahoo.fr>
2020-07-07 11:12:58 +0300

    Remove space

--- src/main/java/models/PingPong.java
+++ src/main/java/models/PingPong.java
@@ -2,7 +2,6 @@ package models;
 
 import java.util.ArrayList;
 import java.util.List;
-
 public class PingPong {
     public ArrayList<Object> runPingPong(int countUpTo){
         ArrayList<Object> result = new ArrayList<Object>();
